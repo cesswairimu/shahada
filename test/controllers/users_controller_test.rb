5cdbcc9462951576a5cb9742f2400213df9e7021
Cess <cessmbuguar@gmail.com>
2017-03-03 22:20:57 +0300

    users test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,5 +5,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_new_url
     assert_response :success
   end
+  test "should get show" do
+    get user_url
+    assert_template 'show'
+  end
+  test "should get index" do
+  get index_path
+  assert_template 'index'
+end
+  
 
 end

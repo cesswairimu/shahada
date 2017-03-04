3e7cbecbe208737405555cfe235baaa6ca2d44f9
Cess <cessmbuguar@gmail.com>
2017-03-04 22:43:44 +0300

    user resources and signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  def create
+  end
+  def show
+  end
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
-
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

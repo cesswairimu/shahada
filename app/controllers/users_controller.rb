0bab214cab571f3c493ca9b4f00bdd75b6b43dc8
Cess <cessmbuguar@gmail.com>
2017-03-05 23:06:08 +0300

    create and show users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    end
+
   end
   def show
+    @user = User.find(params[:id])
   end
   def index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit( :reg_no, :f_name, :l_name, :email,
+                                 :phone, :school, :password, :password_confirmation )
   end
 end

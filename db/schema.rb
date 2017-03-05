cb5428ea8c00bba80a5b8b6c1882fa17eaed0b69
Cess <cessmbuguar@gmail.com>
2017-03-05 23:05:49 +0300

    password field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312191919) do
+ActiveRecord::Schema.define(version: 20170312191920) do
 
   create_table "users", force: :cascade do |t|
     t.string   "reg_no"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20170312191919) do
     t.string   "email"
     t.string   "phone"
     t.string   "school"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end

d513104d9d7efa7853690434a0b82d3e4ad2b2a1
Cess <cessmbuguar@gmail.com>
2017-03-06 23:36:35 +0300

    rendering error form messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
     end
-
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def index
+    @users = User.all
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  NUMBER = /\d[0-9]\)*\z/
+  REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :f_name, presence: true, length: { maximum: 10 }
+  validates :l_name, presence: true, length: { maximum: 10 }
+  validates :reg_no, presence: true
+  validates :email, presence: true, length: { maximum: 26 }, format:
+    { with: REGEX }
+  validates :password, presence:true, length: { minimum: 6 }
+  validates :phone, presence:true, format: { with:NUMBER }
   has_secure_password
 end

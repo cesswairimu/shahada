4264629320c464522710066858deb7e05c61e07a
Cess <cessmbuguar@gmail.com>
2017-04-08 16:47:25 +0300

    new question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,21 @@
 class QuestionsController < ApplicationController
   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(quiz_params)
+    if @question.save
+      flash[:success] = "You have posted your question"
+      redirect_to @question
+    else
+      flash[:nasty] = "Check your inputs and try again"
+      render 'new'
+    end
+  end
+
+  private
+  def quiz_params
+    params.require(:question).permit(:title, :body, :category, :answer, :tag)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :lecturers
+  resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

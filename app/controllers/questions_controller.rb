dc1a36a725ea084a8daf4266d85f249a720e7974
Cess <cessmbuguar@gmail.com>
2017-04-14 00:18:13 +0300

    edit with answer update only

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,9 +17,26 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(quiz_params)
+      flash[:success] = "Your Answer was recorded"
+      redirect_to @question
+    else
+      flash[:nasty] = "Error submitting your answer"
+      render 'edit'
+    end
+  end
+
   def index
     @questions = Question.all
   end
+
   private
   def quiz_params
     params.require(:question).permit(:title, :body, :category, :answer, :tag)

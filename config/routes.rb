2082082ba8d02dcc53739987d732c12cfcc107ae
Cess <cessmbuguar@gmail.com>
2017-05-01 18:47:53 +0300

    logins routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,10 @@ Rails.application.routes.draw do
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users
+  get    'signin' => 'logins#new'
+  post   'signin'  => 'logins#create'
+  delete 'signout' => 'logins#delete'
+   resources :users
   resources :lecturers
   resources :assignments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

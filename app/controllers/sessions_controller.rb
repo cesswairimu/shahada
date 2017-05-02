8bb50ceeb5ba4fd4c320eba105efd0ccb42c520c
Cess <cessmbuguar@gmail.com>
2017-05-02 08:24:54 +0300

    remove lecturer login from session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(email: params[:session][:email])||
-    if user && user.authenticate(params[:session][:password])||
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
       flash[:success] = "Welcome back "
       log_in user 
       redirect_to user 

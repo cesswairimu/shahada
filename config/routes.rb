78e1420f813e77847aea4099e1f9e119563cd7bf
Cess <cessmbuguar@gmail.com>
2017-07-22 22:39:53 +0300

    redirect to home page if page not found

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :lecturers
   resources :questions
   resources :assignments
+  get "*path" => redirect("/")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

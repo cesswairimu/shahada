53e36658333d84b140c813777ca713f2c8a3ed0b
Cess <cessmbuguar@gmail.com>
2017-08-30 21:33:24 +0300

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,4 @@ Rails.application.routes.draw do
   resources :questions
   resources :assignments
   get "*path" => redirect("/")
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

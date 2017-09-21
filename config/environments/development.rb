a86e981eb7f00c3bfc12ecea2434b920bf46fd15
Cess <cessmbuguar@gmail.com>
2017-09-21 23:24:29 +0300

    redis resources

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,20 +13,18 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :redis_store,{
+      host: 'localhost',
+      post: 6379,
+      db: 0,
+      namespace: '056redis'
+    }
     config.public_file_server.headers = {
       'Cache-Control' => 'public, max-age=172800'
     }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
-
-  # Don't care if the mailer can't send.
+    # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false

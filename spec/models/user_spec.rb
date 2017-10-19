f1fa8c62c037a1712ad5a58b92c390216573ccc7
Cess <cessmbuguar@gmail.com>
2017-10-20 01:55:48 +0300

    refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,41 +1,42 @@
 require 'rails_helper'
 
 describe User do
+
   it 'has a valid factory' do
-    expect(FactoryGirl.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it 'is valid with a f_name, l_name, phone, reg_no and email' do
-  user = FactoryGirl.create(:user)
+  user = create(:user)
   expect(user).to be_valid
   end
 
   it 'is invalid without an email' do
-    user = User.new(email: nil)
+    user = build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
   it 'is invalid without f_name'do
-    user = User.new(f_name: nil)
+    user = build(:user, f_name: nil)
     user.valid?
     expect(user.errors[:f_name]).to include("can't be blank")
   end
 
   it 'is invalid without l_name' do
-    user = User.new(l_name: nil) 
+    user = build(:user, l_name: nil)
     user.valid? 
     expect(user.errors[:l_name]).to include("can't be blank")
   end 
 
   it 'is invalid without reg_no' do
-    user = User.new(reg_no: nil)
+    user = build(:user, reg_no: nil)
     user.valid?
     expect(user.errors[:reg_no]).to include("can't be blank") 
   end
 
   it 'is invalid without phone' do
-    user = User.new(phone: nil)
+    user = build(:user, phone: nil)
     user.valid?
     expect(user.errors[:phone]).to include("can't be blank")
   end

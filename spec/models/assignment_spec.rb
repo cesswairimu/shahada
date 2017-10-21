57ae7d3fe9298bcb5c344504e5269232ae931e56
Cess <cessmbuguar@gmail.com>
2017-10-21 12:43:00 +0300

    tests for assignment model

---
+++ spec/models/assignment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+describe Assignment do
+
+it 'must have a lecturer id' do 
+  skip
+  assignment = build(:assignment, lecturer_id: nil)
+  expect(assignment.errors[:lecturer]).to include('Lecturer must exist')
+end
+
+it 'has a valid factory' do
+  expect(build(:assignment)).to be_valid
+end
+end

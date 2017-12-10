f6e620232e9ca035e9e084ed864e8e769437066e
Cess <cessmbuguar@gmail.com>
2017-12-10 21:20:29 +0300

    add activation methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_create :create_activation
+  before_save :downcase_email
   NUMBER = /\d[0-9]\)*\z/
   REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :f_name, presence: true, length: { maximum: 10 }
@@ -19,4 +21,24 @@ class User < ApplicationRecord
   def name
     [f_name, l_name].join(' ')
   end
+
+  private
+  def create_activation
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def downcase_email
+    self.email = email.downcase
+  end
+def digest(string)
+  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+  BCrypt::Password.create(string, cost: cost)
+end
+# Returns a random token.
+def new_token
+SecureRandom.urlsafe_base64
+end
+
 end

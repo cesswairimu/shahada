88db186491cd4fc98bbedefa7a831a1ba0f1c1ea
Cess <cessmbuguar@gmail.com>
2018-01-28 03:52:20 +0300

    remove cookies

--- app/helpers/logins_helper.rb
+++ app/helpers/logins_helper.rb
@@ -6,6 +6,7 @@ module LoginsHelper
   def current_lecturer
     if (lecturer_id = session[:lecturer_id])
       @current_lecturer ||= Lecturer.find_by(id: lecturer_id)
+      lecturer = Lecturer.find_by(id: lecturer_id)
     elsif lecturer && lecturer.authenticated?
       log_in lecturer
       @current_lecturer = lecturer

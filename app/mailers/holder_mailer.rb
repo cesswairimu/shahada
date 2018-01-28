83b48dca76517c32418b73b05ae0a569fb14dfdd
Cess <cessmbuguar@gmail.com>
2018-01-28 03:42:50 +0300

    default from mail

--- app/mailers/holder_mailer.rb
+++ app/mailers/holder_mailer.rb
@@ -3,12 +3,12 @@ class HolderMailer < ApplicationMailer
 
     def account_activation(holder)
       @holder = holder
-      mail to: holder.email, subject: "Account activation"
+      mail to: @holder.email, subject: "Account activation"
     end
 
     def password_reset
-      @greeting = "Hello"
-      mail to: "to@example.org"
+      #@greeting = "Hello"
+      #mail to: "to@example.org"
     end
 
 end

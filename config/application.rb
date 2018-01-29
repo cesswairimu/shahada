6b6dcb8f327eea3c25bf8aa02ffc29bf100a059b
Cess <cessmbuguar@gmail.com>
2018-01-29 03:52:58 +0300

    mailers configurations

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Shahada
       #generate factories instead of fixtures
         g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
-        
+    config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   end
 end

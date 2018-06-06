8368cc341be9d4f7340736a0a6d0f29d3cebf1a0
Cess <cessmbuguar@gmail.com>
2018-06-06 16:28:30 +0300

    :unamused:

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,11 @@
-class School < ActiveRecord::Naming
-
+class School
+MATH = 'math'.freeze
+COMPUTING = 'computing'.freeze
+MEDICINE = 'medicine'.freeze
+ECONOMICS = 'economics'.freeze
+HISTORY = 'history'.freeze
+POLITICS = 'politics'.freeze
+ART = 'art'.freeze
+MUSIC = 'music'.freeze
+LAW = 'law'.freeze
 end

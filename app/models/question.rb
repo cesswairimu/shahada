db7c4df078d5d150e1473159639fe7637038c542
Cess <cessmbuguar@gmail.com>
2018-09-24 12:48:07 +0300

    relationships

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ validates :title, presence: true, length: {maximum: 100}
 validates :body, presence: true
 validates :category, presence:true
 validates :tag, presence:true
+has_many :answers
 
   CAT= ["Networking", "Programming", "Database", "Calculus", "Medicine", "Statistics"]
 end
